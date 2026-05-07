c3fb7a1cd615695c295cc6fd892673844b511915
Kagan Kuscu <kuscukagan@gmail.com>
2026-05-08 02:02:20 +0300

    Installing oh my zsh and refactor installing package to mytools

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 echo "Installing packaging"
-sudo pacman -S --needed - < packages/pkglist.txt
+# sudo pacman -S --needed - < packages/pkglist.txt
+sudo pacman -S --needed - < packages/mytools.txt
 
 echo "Installing AUR packaging"
 yay -S --needed - < packages/aur-pkglist.txt
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Oh My Zsh installing"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 echo "Creating links with stow"
 cd ~/.dotfiles
