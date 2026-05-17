61b3a7cd04258a6b0bd2d1e4756f1c0484814def
Kagan Kuscu <kuscukagan@gmail.com>
2026-05-17 10:44:40 +0300

    Zsh autosuggestions and syntax-highligthing plugin added and install.sh uptaded.

--- install.sh
+++ install.sh
@@ -9,11 +9,24 @@ yay -S --needed - < packages/aur-pkglist.txt
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Oh My Zsh installing"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 echo "Creating links with stow"
 cd ~/.dotfiles
 stow git zsh kitty starship helix yazi opencode
+
+zsh_plugin=/usr/share/zsh/plugins
+
+if [ -d "$zsh_plugin/zsh-autosuggestions" ]; then
+  echo "Zsh Autosuggestion moving..."
+  sudo mv "$zsh_plugin/zsh-autosuggestions" "$HOME/.oh-my-zsh/plugins"
+fi
+
+if [ -d "$zsh_plugin/zsh-syntax-highlighting" ]; then
+  echo "Zsh Syntax Highlihting moving..."
+  sudo mv "$zsh_plugin/zsh-syntax-highlighting" "$HOME/.oh-my-zsh/plugins"
+fi
+
 
 echo "Finished! 🎉"
